d17f567c6fcacaae5251df352fab6cc76614aabd
mariafatima3 <134549281+mariafatima3@users.noreply.github.com>
2025-09-09 17:35:45 +0000

    cap_1_quest_10

---
+++ cap_1_quest_1/cap_1_quest_3/cap_1_quest_4/cap_1_quest_5/cap_1_quest_7/cap_1_quest_8/cap_1_quest_9/cap_1_quest_10/main.py
@@ -0,0 +1,16 @@
+# 10) Um desenvolvedor de jogos quer criar um objeto que guarde as configurações de um jogador, como resolucao_tela (ex: "1920x1080"), volume_audio (um inteiro de 0 a 100) e 
+# dificuldade (ex: "Fácil"). Tarefa: Crie uma classe ConfiguracaoJogo que armazene os três atributos (resolucao_tela, volume_audio, dificuldade) ao ser instanciada.
+
+class ConfiguracaoJogo:
+    def __init__(self, tela, audio, dificuldade):
+
+        self.resolucao_tela = tela
+        self.volume_audio = audio
+        self.dificuldade = dificuldade
+        
+configuracao = ConfiguracaoJogo( "2560x1440", 55 , "Fácil")
+
+
+print(configuracao.resolucao_tela)
+print(configuracao.volume_audio)
+print(configuracao.dificuldade )
